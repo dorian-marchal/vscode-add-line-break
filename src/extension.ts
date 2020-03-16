5d709ff05612d0d1ca20903803b8f0ba2931ef12
Dorian Marchal <marchal.dorian@gmail.com>
2020-03-16 09:52:13 +0100

    Bump extension base and dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,50 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // If direction is 'before', add a line before the selected lines
 // else, add a new line after the selected lines.
-const addLineBreak = (direction) => {
-    const editor = vscode.window.activeTextEditor;
+const addLineBreak = (direction: "before" | "after") => {
+  const editor = vscode.window.activeTextEditor;
 
-    if (!editor) {
-        return;
-    }
+  if (!editor) {
+    return;
+  }
 
-    const selections = editor.selections;
-    const newLinePositions = [];
+  const selections = editor.selections;
+  const newLinePositions = [] as vscode.Position[];
 
-    selections.forEach((selection, i) => {
-        const { start, end } = selection;
+  selections.forEach(selection => {
+    const { start, end } = selection;
 
-        newLinePositions.push((direction === 'before')
-            ? (new vscode.Position(start.line, 0))
-            : (new vscode.Position(end.line + 1, 0)));
-    });
-
-    editor.edit((editBuilder) =>
-        newLinePositions.forEach((newLinePosition) =>
-            editBuilder.insert(newLinePosition, '\n')
-        )
+    newLinePositions.push(
+      direction === "before"
+        ? new vscode.Position(start.line, 0)
+        : new vscode.Position(end.line + 1, 0)
     );
+  });
+
+  editor.edit(editBuilder =>
+    newLinePositions.forEach(newLinePosition =>
+      editBuilder.insert(newLinePosition, "\n")
+    )
+  );
 };
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('"add-line-break" is now active.');
+  console.log('"add-line-break" is now active.');
 
-    context.subscriptions.push(vscode.commands.registerCommand(
-        'extension.addNewLineBeforeSelection',
-        () => addLineBreak('before')
-    ));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.addNewLineBeforeSelection", () =>
+      addLineBreak("before")
+    )
+  );
 
-    context.subscriptions.push(vscode.commands.registerCommand(
-        'extension.addNewLineAfterSelection',
-        () => addLineBreak('after')
-    ));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.addNewLineAfterSelection", () =>
+      addLineBreak("after")
+    )
+  );
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
